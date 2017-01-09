2e4d4a5dc4ed7638c565e99ebcf9e3817e281a3c
AmalQB <amalv@qburst.com>
2017-01-09 09:33:35 +0530

    Adding Deployment

--- lambda/index.js
+++ lambda/index.js
@@ -3,6 +3,7 @@
 var AWS = require('aws-sdk');
 var YAML = require('yamljs');
 exports.handler = (event, context, callback) => {
+    console.log("Starting Lambda Execution with Event Handler");
     //Configuring AWS
     var AWS = require('aws-sdk');
 
@@ -16,13 +17,14 @@ exports.handler = (event, context, callback) => {
         Key: event['CodePipeline.job'].data.inputArtifacts[0].location.s3Location.objectKey
     };
     //Getting pipeline ID
+    console.log(params);
     var jobId = event["CodePipeline.job"].id;
-    AWS.config.update({region: 'ap-southeast-1'});
-    var ecs = new AWS.ECS();
     var s3 = new AWS.S3({
         maxRetries: 10,
         signatureVersion: "v4"
     });
+    AWS.config.update({region: 'ap-southeast-1'});
+    var ecs = new AWS.ECS();
     const unzipCommand = "rm -rf /tmp/artifacts && mkdir -p /tmp/artifacts && unzip /tmp/artifact.zip -d /tmp/artifacts"
     // Notify AWS CodePipeline of a successful job
     var putJobSuccess = function(message) {
@@ -60,21 +62,29 @@ exports.handler = (event, context, callback) => {
     if (err) putJobFailure(err);
     else {
         //writing the artifacts to the /tmp/ , we have access to only this directory
+        console.log("Writing the Zip File");
         fs.writeFile("/tmp/artifact.zip", data.Body, function(err) {
             if (err) putJobFailure(err);
             else {
+                console.log("Executing the Unzip command");
                 const child = exec(unzipCommand, (error) => {
-                    var serviceDefinition = require('/tmp/artifacts/service.yaml');
-                    AWS.config.update({region: 'us-east-1'});
-                    var cloudformation = new AWS.CloudFormation();
-                    var params = {
-                      StackName: 'sampleawssdks3', /* required */
-                      TemplateBody: JSON.stringify(serviceDefinition),
+                    if (error) putJobFailure(error);
+                    else {
+                      var serviceDefinition = YAML.load('/tmp/artifacts/ecs/service.yaml');
+                      console.log(JSON.stringify(serviceDefinition));
+                      //https://s3.amazonaws.com/cftemplates-us-east/Archive.zip
+                      console.log(JSON.stringify(serviceDefinition.Resources));
+                      AWS.config.update({region: 'ap-southeast-1'});
+                      var cloudformation = new AWS.CloudFormation();
+                      var params = {
+                        StackName: 'sampleawssdks3', /* required */
+                        TemplateBody: JSON.stringify(serviceDefinition),
+                      }
+                      cloudformation.createStack(params, function (err, data) {
+                        if (err) console.log(err, err.stack); // an error occurred
+                        else     console.log(data);           // successful response
+                      });
                     }
-                    cloudformation.createStack(params, function (err, data) {
-                      if (err) console.log(err, err.stack); // an error occurred
-                      else     console.log(data);           // successful response
-                    });
                 });
                 //print the output of child process
                 child.stdout.on('data', console.log);
